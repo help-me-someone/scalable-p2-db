0cfb407098fc37ad2691fecbddf815d5a7a110a0
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-10-21 18:21:42 +0700

    what was I doing

--- main.go
+++ main.go
@@ -49,8 +49,6 @@ func main() {
 	router.GET("/video/:key", cmw.Attach(handlers.GetVideo))
 	router.GET("/rank/video/:rank", cmw.Attach(handlers.GetVideoByRank))
 
-	router.GET("/whoami/videos", cmw.Attach(handlers.WhoAmIVideosHandler))
-
 	// Expects: "username", "hashed_password" in JSON.
 	router.POST("/user", cmw.Attach(handlers.CreateUser))
 
